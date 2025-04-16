5dc0fd561b6049dd71512bffa466a36686e610cc
jino <kjo9148@gmail.com>
2025-04-16 23:48:48 +0900

    base

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
-  시발
+  <div id="app">
+    <h1>🍹 Welcome to My Drink Store!</h1>
+    <p>이곳은 나만의 음료 가게입니다.</p>
+  </div>
 </template>
+
+<script setup>
+// 아무 설정도 필요 없음 (script setup 방식)
+</script>
+
+<style>
+#app {
+  font-family: sans-serif;
+  text-align: center;
+  padding: 2rem;
+}
+</style>
